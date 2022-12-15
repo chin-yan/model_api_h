3b384531c36d9f397265795b40f8cd8053dff0ba
chin-yan <72589447+chin-yan@users.noreply.github.com>
2022-12-15 14:16:15 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,18 +8,12 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/test', methods=['GET'])
-def getResult():
-    input = np.array([[5.5, 2.4, 2.7, 1.]])
-    result = model.predict(input)
-    return jsonify({'result': str(result)})
-
 @app.route('/predict', methods=['POST'])
 def postInput():
+    # 取得前端傳過來的數值
     insertValues = request.get_json()
-    x1=insertValues['file_name']
-    globals.img = str(x1)
-
-    result = model.predict(globals.img)
-    return str(result)
-    #return jsonify({'return': str(globals.img)})
+    x1=insertValues['_id']
+    input = str(x1)
+    
+    result = model.predict(input)
+    return jsonify({'return': str(result)})
